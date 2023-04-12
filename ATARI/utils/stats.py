f371dea2d756329d410d19038029569377ac304b
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-04-12 09:30:51 -0400

    Add logic to catch incorrect sized vectors

--- ATARI/utils/stats.py
+++ ATARI/utils/stats.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.linalg import inv
 
 
-def calc_chi2(a,b,cov):
+def chi2_val(a,b,cov):
     """
     Calculates the $chi^2$ statistic between fit and data.
 
@@ -15,11 +15,21 @@ def calc_chi2(a,b,cov):
     cov : ndarray
         Covariance matrix for data in vector b.
     """
+    # cast into numpy arrays
+    a = np.atleast_2d(a)
+    b = np.atleast_2d(b)
+    cov = np.atleast_2d(cov)
+
     # re-orient if needed
     if np.shape(a)[0] != 1:
         a = a.T
+    if np.shape(a)[0] != 1:
+        raise ValueError("The vector passed for a does not have the correct dimensions")
     if np.shape(b)[0] != 1:
         b = b.T
+    if np.shape(b)[0] != 1:
+        raise ValueError("The vector passed for b does not have the correct dimensions")
+
     # check that a and b are the same size
     if np.shape(b) != np.shape(a):
         raise ValueError("Input vectors are not the same size.")

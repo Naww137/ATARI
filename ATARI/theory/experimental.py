0e29d7fcf1cce1cfdd731fe6d1fefd33e78b70ea
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-04-27 14:14:17 -0400

    introduce theory/experimental

---
+++ ATARI/theory/experimental.py
@@ -0,0 +1,60 @@
+import numpy as np
+from scipy.linalg import block_diag
+
+
+def trans_2_xs(T, n, n_unc=0, CovT=None):
+    """
+    Converts pointwise transmission data to total cross section data. 
+    If covariance data is supplied, first order linear propagation is used to approximate the cross section covariance matrix.
+    If the transmission value is below 0, and nan-type is returned for the corresponding cross section.
+
+    Parameters
+    ----------
+    T : array-like
+        Vector of transmission values.
+    n : float
+        Target thickness in atoms/bn-cm.
+    n_unc : float
+        Uncertainty in the target thickness, default 0.
+    CovT : array-like or None
+        Transmission covariance matrix, default None.
+
+    Returns
+    -------
+    array-like
+        Pointwise cross section values.
+    array-like
+        Cross section covariance matrix
+    """
+    
+    xs_exp = (-1/n)*np.log(T)
+
+    if CovT is not None:
+        dXi_dn = (1/n**2) * np.log(T)
+        dXi_dT = (-1/n) * (1/T)
+        Jac = np.vstack((np.diag(dXi_dT),dXi_dn))
+        Cov = block_diag(CovT,n_unc**2)
+        CovXS = Jac.T @ Cov @ Jac
+    else:
+        CovXS = None
+
+    return xs_exp, CovXS
+
+
+def xs_2_trans(xs_tot, n):
+    """
+    Converts pointwise total cross section data to transmission data.
+
+    Parameters
+    ----------
+    xs_tot : array-like
+        Total cross section.
+    n : float
+        Target thickness in atoms/bn-cm.
+
+    Returns
+    -------
+    array-like
+        Pointwise transmission.
+    """
+    return np.exp(-n*xs_tot)

70d31f589be27eb536acbff78f3fec9f35f10aaa
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-04-27 14:15:33 -0400

    more utils

--- ATARI/utils/misc.py
+++ ATARI/utils/misc.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from ATARI.theory.xs import SLBW
+from ATARI.utils.atario import fill_resonance_ladder
 
 def fine_egrid(energy, ppeV):
     """
@@ -17,7 +18,37 @@ def fine_egrid(energy, ppeV):
     ndarray
         Array of energy points.
     """
+    energy = np.array(energy).flatten()
     minE = min(energy); maxE = max(energy)
     n = int((maxE - minE)*ppeV)
     new_egrid = np.linspace(minE, maxE, n)
     return new_egrid
+
+def calc_xs_on_fine_egrid(E, ppeV, particle_pair, resonance_ladder):
+    """
+    Calculates total cross section on a fine energy grid.
+
+    Parameters
+    ----------
+    E : array-like
+        Coarse energy grid, used to define energy domain.
+    ppeV : int
+        Requested points per eV on fine grid.
+    particle_pair : ATARI.syndat.Particle_Pair
+        ATARI.syndat particle pair object
+    resonance_ladder : DataFrame
+        Pandas DataFrame containing resonance ladder information.
+
+    Returns
+    -------
+    array-like
+        New, fine energy grid.
+    array-like
+        Total cross section on new, fine energy grid.
+    """
+
+    fineE = fine_egrid(E, ppeV)
+    resonance_ladder = fill_resonance_ladder(resonance_ladder,particle_pair)
+    xs_tot_fine, _, _ = SLBW(fineE, particle_pair, resonance_ladder)
+        
+    return fineE, xs_tot_fine

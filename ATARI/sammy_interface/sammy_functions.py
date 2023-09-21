b38c32a1898c583d6b2341ff15a9a4b3d1b7bf75
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-09-21 08:25:43 -0400

    YW fitting with sammy

--- ATARI/sammy_interface/sammy_functions.py
+++ ATARI/sammy_interface/sammy_functions.py
@@ -35,7 +35,7 @@ def readlst(filepath):
     DataFrame
         DataFrame with headers.
     """
-    if filepath.endswith('.LST'):
+    if filepath.endswith('.LST') or filepath.endswith('.lst'):
         df = pd.read_csv(filepath, delim_whitespace=True, names=['E','exp_xs','exp_xs_unc','theo_xs','theo_xs_bayes','exp_trans','exp_trans_unc','theo_trans', 'theo_trans_bayes'])
     else:
         df = pd.read_csv(filepath, delim_whitespace=True, names=['E','exp_dat','exp_dat_unc'])
@@ -312,90 +312,7 @@ def create_sammyinp(filename='sammy.inp', \
         f.close()
         
     return
-    
-# =============================================================================
-# 
-# =============================================================================
-# def read_sammy_par(filename, calculate_average):
-#     """
-#     Reads sammy.par file and calculates average parameters.
 
-#     _extended_summary_
-
-#     Parameters
-#     ----------
-#     filename : _type_
-#         _description_
-#     calculate_average : bool
-#         Whether or not to calculate average parameters.
-
-#     Returns
-#     -------
-#     DataFrame
-#         Contains the average parameters for each spin group
-#     DataFrame
-#         Contains all resonance parameters for all spin groups
-#     """
-
-#     energies = []; spin_group = []; nwidth = []; gwidth = []
-#     with open(filename,'r') as f:   
-#         readlines = f.readlines()
-#         in_res_dat = True
-#         for line in readlines:   
-
-#             try:
-#                 float(line.split()[0]) # if first line starts with a float, parameter file starts directly with resonance data
-#             except:
-#                 in_res_dat = False # if that is not the case, parameter file has some keyword input before line "RESONANCES" then resonance parameters
-
-#             if line.startswith(' '):
-#                 in_res_dat = False  
-#             if in_res_dat:
-#                 if line.startswith('-'):
-#                     continue #ignore negative resonances
-#                 else:
-#                     splitline = line.split()
-#                     energies.append(float(splitline[0]))
-#                     gwidth.append(float(splitline[1]))
-#                     nwidth.append(float(splitline[2]))
-#                     spin_group.append(float(splitline[-1]))
-#             if line.startswith('RESONANCE'):
-#                 in_res_dat = True
-
-                
-                
-#     Gg = np.array(gwidth); Gn = np.array(nwidth); E = np.array(energies); jspin = np.array(spin_group)
-#     df = pd.DataFrame([E, Gg, Gn, jspin], index=['E','Gg','Gn','jspin']); df = df.transpose()
-    
-#     if calculate_average:
-#         #avg_widths = df.groupby('jspin', as_index=False)['Gg','Gn'].mean() 
-#         gb = df.groupby('jspin')    
-#         list_of_dfs=[gb.get_group(x) for x in gb.groups]
-        
-#         avg_df = pd.DataFrame(index=df['jspin'].unique(),columns=['dE','Gg','Gn'])
-
-#         for ij, jdf in enumerate(list_of_dfs):
-#             avg_df['dE'][ij+1]=jdf['E'].diff().mean()
-#             avg_df['Gg'][ij+1]=jdf['Gg'].mean()
-#             avg_df['Gn'][ij+1]=jdf['Gn'].mean()
-#     else:
-#         avg_df = ''
-
-#     return avg_df, df
-    
-
-
-# =============================================================================
-# 
-# ============================================================================= 
-# def copy_template_to_runDIR(experimental_corrections, filename, target_dir):
-#     if os.path.splitext(filename)[0] != 'sammy':
-#         file_out = ''.join(('sammy', os.path.splitext(filename)[1]))
-#     else:
-#         file_out = filename
-#     shutil.copy(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sammy_templates', f'{experimental_corrections}', filename), 
-#                 os.path.join(target_dir,file_out))
-#     return
 
 def write_saminp(filepath, 
                 model, bayes, 
@@ -421,7 +338,7 @@ def write_saminp(filepath,
     with open(filepath,'w') as f:
         for line in old_lines:
 
-            if "broadening is not wa" in line.lower():
+            if "broadening is not wa" in line.lower() or np.any([each.lower().startswith("broadening is not wa") for each in alphanumeric]):
                 broadening = False
 
             if line.startswith("%%%alphanumeric%%%"):
@@ -587,3 +504,331 @@ def run_sammy(sammy_INP: SammyInputData, sammy_RTO:SammyRunTimeOptions):
         shutil.rmtree(sammy_RTO.sammy_runDIR)
 
     return sammy_OUT
+
+
+
+
+
+#########################################################
+# run sammy with the YW scheme
+#########################################################
+
+
+# def copy_inptemp_to_runDIR(template_path, target_path):
+#     shutil.copy(template_path, target_path)
+
+
+def make_inputs_for_YW(sammyINP, sammyRTO, datasets, reactions, templates):
+
+    for ds, rxn, tem in zip(datasets, reactions, templates):
+
+        sammyRTO.template = tem
+
+        sammyRTO.inpname = os.path.join(sammyRTO.sammy_runDIR, f"{ds}_initial.inp")
+        fill_runDIR_with_templates(sammyRTO)
+        write_saminp(os.path.join(sammyRTO.sammy_runDIR,f"{ds}_initial.inp"), sammyRTO.model, sammyRTO.solve_bayes, rxn, sammyINP, 
+                                    alphanumeric=["yw"])
+
+        sammyRTO.inpname = os.path.join(sammyRTO.sammy_runDIR, f"{ds}_iter.inp")
+        fill_runDIR_with_templates(sammyRTO)
+        write_saminp(os.path.join(sammyRTO.sammy_runDIR,f"{ds}_iter.inp"), sammyRTO.model, sammyRTO.solve_bayes, rxn, sammyINP, 
+                                    alphanumeric=["yw","Use remembered original parameter values"])
+
+        sammyRTO.inpname = os.path.join(sammyRTO.sammy_runDIR, f"{ds}_plot.inp")
+        fill_runDIR_with_templates(sammyRTO)
+        write_saminp(os.path.join(sammyRTO.sammy_runDIR,f"{ds}_plot.inp"), sammyRTO.model, False, rxn, sammyINP, 
+                                    alphanumeric=[])
+
+    # copy_inptemp_to_runDIR("/Users/noahwalton/Documents/GitHub/ATARI/examples/sammy_template.inp", os.path.join(sammyRTO.sammy_runDIR, "solvebayes_initial.inp"))
+    sammyRTO.inpname = os.path.join(sammyRTO.sammy_runDIR, "solvebayes_initial.inp")
+    fill_runDIR_with_templates(sammyRTO)
+    write_saminp(os.path.join(sammyRTO.sammy_runDIR,"solvebayes_initial.inp"), sammyRTO.model, sammyRTO.solve_bayes, "reaction", sammyINP, 
+                                alphanumeric=["wy", "CHI SQUARED IS WANTED", "USE LEAST SQUARES TO GIVE COVARIANCE MATRIX", "Remember original parameter values", "Take baby steps with Least-Squares method"])#, "Broadening is not wanted"])
+
+
+    # copy_inptemp_to_runDIR("/Users/noahwalton/Documents/GitHub/ATARI/examples/sammy_template.inp", os.path.join(sammyRTO.sammy_runDIR, "solvebayes_iter.inp"))
+    sammyRTO.inpname = os.path.join(sammyRTO.sammy_runDIR, "solvebayes_iter.inp")
+    fill_runDIR_with_templates(sammyRTO)
+    write_saminp(os.path.join(sammyRTO.sammy_runDIR,"solvebayes_iter.inp"), sammyRTO.model, sammyRTO.solve_bayes, "reaction", sammyINP, 
+                                alphanumeric=["wy", "CHI SQUARED IS WANTED", "USE LEAST SQUARES TO GIVE COVARIANCE MATRIX", "Use remembered original parameter values", "Take baby steps with Least-Squares method"])#, "Broadening is not wanted"])
+
+
+
+def make_bash_script_iterate(iterations, dataset_titles, rundir, sammyexe, save_each_step=True):
+
+    with open(os.path.join(rundir, "iterate.zsh"), 'w+') as f:
+
+            for i in range(1, iterations+1):
+
+                stepstr_prior = f'iter{i-1}'
+                if i == 1:
+                    cov = ''
+                    par = 'results/step$1.par'
+                    inp_ext = 'initial'
+                else:
+                    cov = f"iterate/bayes_{stepstr_prior}.cov"
+                    par = f"iterate/bayes_{stepstr_prior}.par"
+                    inp_ext = 'iter'
+
+                # Write commands for each dataset
+                for ds in dataset_titles:
+                    title = f"{ds}_{stepstr_prior}"#step{i}
+                    f.write(f"##################################\n# Generate YW for {ds}\n")
+                    f.write(f"{sammyexe}<<EOF\n{ds}_{inp_ext}.inp\n{par}\n{ds}.dat\n{cov}\n\nEOF\n")
+                    f.write(f"""mv -f SAMMY.LPT "iterate/{title}.lpt" \nmv -f SAMMY.ODF "iterate/{title}.odf" \nmv -f SAMMY.LST "iterate/{title}.lst" \nmv -f SAMMY.YWY "iterate/{title}.ywy" \n""")    
+
+                # write commands for bayes solve using all datasets
+                dataset_inserts = "\n".join([f"iterate/{ds}_{stepstr_prior}.ywy" for ds in dataset_titles])
+                title = f"bayes_iter{i}"
+                f.write(f"#######################################################\n# Run Bayes for {title}\n#\n#######################################################\n")
+                f.write(f"{sammyexe}<<eod\nsolvebayes_{inp_ext}.inp\n{par}\ndummy.dat\n{dataset_inserts}\n\n{cov}\n\neod\n")
+                f.write(f"""mv -f SAMMY.LPT iterate/{title}.lpt \nmv -f SAMMY.PAR iterate/{title}.par \nmv -f SAMMY.COV iterate/{title}.cov \nrm -f SAM*\n""")
+
+            f.write(f"\n\n\n\n############## Copy Iteration Result ###########\nplus_one=$(( $1 + 1 ))\nhead -$(($(wc -l < iterate/bayes_iter{iterations}.par) - 3)) iterate/bayes_iter{iterations}.par > results/step$plus_one.par\n\n")
+            
+            ### After all steps, write commands to make plot for step
+            for ds in dataset_titles:
+                f.write(f"rm REMORI.PAR\n#######################################################\n# Plotting for {ds} final iteration \n#\n#######################################################\n")
+                f.write(f"{sammyexe}<<EOF\n{ds}_plot.inp\nresults/step$plus_one.par\n{ds}.dat\n\nEOF\n\n")
+
+                # write lines to grep chi2
+                f.write("""line_with_chi2=$(grep -i "CUSTOMARY CHI SQUARED DIVIDED" SAMMY.LPT)
+    chi2_string=$(echo "$line_with_chi2" | awk '{ for (i=1; i<=NF; i++) if ($i ~ /[0-9]+(\.[0-9]+)?([Ee][+-]?[0-9]+)?/) print $i }')
+    echo "step $plus_one chi2: $chi2_string" >> chi2.txt\n\n""")
+                
+                if save_each_step:
+                    f.write(f"mv -f SAMMY.LST results/{ds}_step$plus_one.lst\nmv -f SAMMY.LPT results/trans1_step$plus_one.lpt\n")
+
+                f.write("rm -f SAM*\n\n")
+
+                # f.write(f"{sammyexe}<<EOF\n{ds}_plot.inp\ninitial.par\n{ds}.dat\n\nEOF\n")
+                # f.write(f"mv -f SAMMY.LST {ds}_initial.lst\nrm -f SAM*\n")
+
+
+def make_data_for_YW(datasets, dataset_titles, rundir):
+    for d, dt in zip(datasets, dataset_titles):
+        write_samdat(d, None, os.path.join(rundir,f"{dt}.dat"))
+        write_estruct_file(d.E, os.path.join(rundir,"dummy.dat"))
+
+
+def make_bash_script_run(steps, rundir):
+    with open(os.path.join(rundir, "run.zsh"), 'w+') as f:
+        f.write("""# change cwd
+cd "$(dirname "$0")"\n
+# remove existing chi2 log
+rm chi2.txt\n\n
+#### plot prior
+/Users/noahwalton/gitlab/sammy/sammy/build/bin/sammy<<EOF
+trans1_plot.inp
+results/step0.par
+trans1.dat
+
+EOF
+mv -f SAMMY.LST results/trans1_step0.lst
+mv -f SAMMY.LPT results/trans1_step0.lpt
+rm -f SAM*
+#### For each step
+#   1. run iteration for nonlinearities 
+#   2. OPTIONAL: Generate plot for step\n""")
+
+        f.write( "for number in {0..$steps$}".replace("$steps$",str(steps)) )
+
+        f.write("""\ndo
+    ./iterate.zsh $number
+
+    # #######################################################
+    # # Plotting for step - don't need to do each time
+    # /Users/noahwalton/gitlab/sammy/sammy/build/bin/sammy<<EOF
+    # trans1_plot.inp
+    # results/step$number.par
+    # trans1.dat
+
+    # EOF
+    # mv -f SAMMY.LST results/trans1_step$plus_one.lst
+    # mv -f SAMMY.LPT results/trans1_step$plus_one.lpt
+    # rm -f SAM*
+
+
+        # line_with_chi2=$(grep -i "CUSTOMARY CHI SQUARED DIVIDED" results/trans1_step$number.lpt)
+        # chi2_string=$(echo "$line_with_chi2" | awk '{ for (i=1; i<=NF; i++) if ($i ~ /[0-9]+(\.[0-9]+)?([Ee][+-]?[0-9]+)?/) print $i }')
+        # chi2=$(echo "$chi2_string" | bc -l)
+        # # echo "chi2 (float): $chi2"
+        # # # Example: Perform arithmetic operations with chi2
+        # # result=$(echo "$chi2 * 2" | bc -l)
+        # # echo "Result of chi2 * 2: $result"
+
+        # echo "step $number chi2: $chi2_string" >> chi2.txt
+
+    done
+
+    # ./iterate.zsh 0
+
+    # line_with_chi2=$(grep -i "CUSTOMARY CHI SQUARED DIVIDED" SAMMY.LPT)
+    # chi2_string=$(echo "$line_with_chi2" | awk '{ for (i=1; i<=NF; i++) if ($i ~ /[0-9]+(\.[0-9]+)?([Ee][+-]?[0-9]+)?/) print $i }')
+    # chi2=$(echo "$chi2_string" | bc -l)
+    # # echo "chi2 (float): $chi2"
+    # # # Example: Perform arithmetic operations with chi2
+    # # result=$(echo "$chi2 * 2" | bc -l)
+    # # echo "Result of chi2 * 2: $result"
+
+    # echo "step 0 chi2: $chi2_string" >> chi2.txt
+    # # echo  "$chi2"
+
+
+
+
+
+    # ############## Copy Final Result ###########
+    # head -$(($(wc -l < results/bayes_step50.par) - 3)) results/bayes_step50.par > final.par
+
+    # #######################################################
+    # # Plotting for trans1 Final/Initial
+    # #
+    # #######################################################
+    # /Users/noahwalton/gitlab/sammy/sammy/build/bin/sammy<<EOF
+    # trans1_plot.inp
+    # final.par
+    # trans1.dat
+
+    # EOF
+    # mv -f SAMMY.LST trans1_final.lst
+    # rm -f SAM*
+
+    # /Users/noahwalton/gitlab/sammy/sammy/build/bin/sammy<<EOF
+    # trans1_plot.inp
+    # initial.par
+    # trans1.dat
+
+    # EOF
+    # mv -f SAMMY.LST trans1_initial.lst
+    # rm -f SAM*""")
+        
+
+
+def setup_YW_scheme(sammyRTO, sammyINP, datasets, dataset_titles, reactions, templates, 
+                                                                                steps=10,
+                                                                                iterations=5):
+
+    ### clean and make directories
+    try:
+        shutil.rmtree(sammyRTO.sammy_runDIR)
+    except:
+        pass
+    os.mkdir(sammyRTO.sammy_runDIR)
+    os.mkdir(os.path.join(sammyRTO.sammy_runDIR, "results"))
+    os.mkdir(os.path.join(sammyRTO.sammy_runDIR, "iterate"))
+    # for ds in datasets:
+    #     os.mkdir(os.path.join(sammyRTO.sammy_runDIR, "results", ds))
+
+    make_data_for_YW(datasets, dataset_titles, sammyRTO.sammy_runDIR)
+    write_sampar(sammyINP.resonance_ladder, sammyINP.particle_pair, True, 0.1, os.path.join(sammyRTO.sammy_runDIR, "results/step0.par"))
+
+    make_inputs_for_YW(sammyINP, sammyRTO, dataset_titles, reactions, templates)
+    make_bash_script_iterate(iterations, dataset_titles, sammyRTO.sammy_runDIR, sammyRTO.path_to_SAMMY_exe)
+    make_bash_script_run(steps, sammyRTO.sammy_runDIR)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# 
+# =============================================================================
+# def read_sammy_par(filename, calculate_average):
+#     """
+#     Reads sammy.par file and calculates average parameters.
+
+#     _extended_summary_
+
+#     Parameters
+#     ----------
+#     filename : _type_
+#         _description_
+#     calculate_average : bool
+#         Whether or not to calculate average parameters.
+
+#     Returns
+#     -------
+#     DataFrame
+#         Contains the average parameters for each spin group
+#     DataFrame
+#         Contains all resonance parameters for all spin groups
+#     """
+
+#     energies = []; spin_group = []; nwidth = []; gwidth = []
+#     with open(filename,'r') as f:   
+#         readlines = f.readlines()
+#         in_res_dat = True
+#         for line in readlines:   
+
+#             try:
+#                 float(line.split()[0]) # if first line starts with a float, parameter file starts directly with resonance data
+#             except:
+#                 in_res_dat = False # if that is not the case, parameter file has some keyword input before line "RESONANCES" then resonance parameters
+
+#             if line.startswith(' '):
+#                 in_res_dat = False  
+#             if in_res_dat:
+#                 if line.startswith('-'):
+#                     continue #ignore negative resonances
+#                 else:
+#                     splitline = line.split()
+#                     energies.append(float(splitline[0]))
+#                     gwidth.append(float(splitline[1]))
+#                     nwidth.append(float(splitline[2]))
+#                     spin_group.append(float(splitline[-1]))
+#             if line.startswith('RESONANCE'):
+#                 in_res_dat = True
+
+                
+                
+#     Gg = np.array(gwidth); Gn = np.array(nwidth); E = np.array(energies); jspin = np.array(spin_group)
+#     df = pd.DataFrame([E, Gg, Gn, jspin], index=['E','Gg','Gn','jspin']); df = df.transpose()
+    
+#     if calculate_average:
+#         #avg_widths = df.groupby('jspin', as_index=False)['Gg','Gn'].mean() 
+#         gb = df.groupby('jspin')    
+#         list_of_dfs=[gb.get_group(x) for x in gb.groups]
+        
+#         avg_df = pd.DataFrame(index=df['jspin'].unique(),columns=['dE','Gg','Gn'])
+
+#         for ij, jdf in enumerate(list_of_dfs):
+#             avg_df['dE'][ij+1]=jdf['E'].diff().mean()
+#             avg_df['Gg'][ij+1]=jdf['Gg'].mean()
+#             avg_df['Gn'][ij+1]=jdf['Gn'].mean()
+#     else:
+#         avg_df = ''
+
+#     return avg_df, df
+    
+
+
+# =============================================================================
+# 
+# ============================================================================= 
+# def copy_template_to_runDIR(experimental_corrections, filename, target_dir):
+#     if os.path.splitext(filename)[0] != 'sammy':
+#         file_out = ''.join(('sammy', os.path.splitext(filename)[1]))
+#     else:
+#         file_out = filename
+#     shutil.copy(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sammy_templates', f'{experimental_corrections}', filename), 
+#                 os.path.join(target_dir,file_out))
+#     return

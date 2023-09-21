66413fe7ca94c29d32a8c1c558e94352029ed4d5
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-09-21 09:23:28 -0400

    reorg

---
+++ Fitting/noah_dev/gen_data.py
@@ -0,0 +1,95 @@
+
+from matplotlib.pyplot import *
+import numpy as np
+import pandas as pd
+import os
+# import nuctools
+
+from ATARI.syndat.particle_pair import Particle_Pair
+from ATARI.syndat.experiment import Experiment
+from ATARI.syndat.MMDA import generate
+from ATARI.theory.xs import SLBW
+from ATARI.sammy_interface import sammy_interface, sammy_classes, sammy_functions
+
+
+
+ac = 0.81271    # scattering radius in 1e-12 cm 
+M = 180.948030  # amu of target nucleus
+m = 1           # amu of incident neutron
+I = 3.5         # intrinsic spin, positive parity
+i = 0.5         # intrinsic spin, positive parity
+l_max = 1       # highest order l-wave to consider
+
+
+
+experiment_parameters = {'bw': {'val':0.1024,    'unc'   :   0}} 
+input_options = {'Add Noise': True,
+            'Calculate Covariance': False,
+            'Sample TURP': True}
+
+energy_grid = [75, 125]
+exp = Experiment(energy_grid, 
+                        input_options=input_options, 
+                        experiment_parameters=experiment_parameters)
+
+Gg_DOF = 10
+spin_groups = [ (3.0,1,0) ]
+res_par_avg = make_res_par_avg(D_avg = 8.79, 
+                            Gn_avg= 0.658, #0.658, 
+                            n_dof = 1, 
+                            Gg_avg = 64.0, 
+                            g_dof = Gg_DOF, 
+                            print = False)
+
+average_parameters = {'3.0':res_par_avg}
+Ta_pair = Particle_Pair( ac, M, m, I, i, l_max,
+                                input_options={},
+                                spin_groups=spin_groups,
+                                average_parameters=average_parameters )   
+
+
+
+sammyRTO = sammy_classes.SammyRunTimeOptions(
+    path_to_SAMMY_exe = '/Users/noahwalton/gitlab/sammy/sammy/build/bin/sammy',
+    model = 'XCT',
+    reaction = 'total',
+    solve_bayes = False,
+    inptemplate= "allexptot_1sg.inp",
+    energy_window = None,
+    sammy_runDIR = 'SAMMY_runDIR_1',
+    keep_runDIR = True,
+    shell = 'zsh'
+    )
+
+sammyINP = sammy_classes.SammyInputData(
+    particle_pair = Ta_pair,
+    resonance_ladder = resonance_ladder,
+    energy_grid = exp.energy_domain,
+    temp = 304.5,
+    FP=35.185,
+    frac_res_FP=0.049600,
+    target_thickness=0.067166)
+
+
+
+case_file = '/Users/noahwalton/Documents/GitHub/ATARI/Fitting/noah_dev/fit_w_sammy/data.hdf5'  # if using hdf5
+
+dataset_range = (0, 500)
+spin_groups = [ (3.0,1,0) ]
+Ta_pair = Particle_Pair( ac, M, m, I, i, l_max,
+                                input_options={},
+                                spin_groups=spin_groups,
+                                average_parameters=average_parameters )    
+
+samples_not_generated = generate(Ta_pair, exp, 
+                                        'sammy', 
+                                        dataset_range, 
+                                        case_file,
+                                        fixed_resonance_ladder=None, 
+                                        open_data=None,
+                                        vary_Erange=None,
+                                        use_hdf5=True,
+                                        overwrite = True,
+                                        sammy_RTO=sammyRTO,
+                                        sammy_INP=sammyINP
+                                                                    )

8a42926f5e1e6631c06aa47b7fbf59625e512f29
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-10-12 08:07:30 -0400

    update for multiple spin groups

--- ATARI/theory/resonance_statistics.py
+++ ATARI/theory/resonance_statistics.py
@@ -5,9 +5,9 @@ from scipy.stats.distributions import chi2
 def getD(xi,res_par_avg):    
     return res_par_avg['<D>']*2*np.sqrt(np.log(1/(1-xi))/np.pi)
 
-def make_res_par_avg(D_avg, Gn_avg, n_dof, Gg_avg, g_dof, print):
-    
-    res_par_avg = {'<D>'  :D_avg,'<Gn>' :Gn_avg,'n_dof':n_dof,'<Gg>' :Gg_avg,'g_dof':g_dof}
+def make_res_par_avg(J_ID, D_avg, Gn_avg, n_dof, Gg_avg, g_dof, print):
+
+    res_par_avg = {'J_ID':J_ID, '<D>':D_avg, '<Gn>':Gn_avg, 'n_dof':n_dof, '<Gg>':Gg_avg, 'g_dof':g_dof}
 
     res_par_avg['D01']  = getD(0.01,res_par_avg)
     res_par_avg['D99']  = getD(0.99,res_par_avg)

2a97e45075ba3518b48f3148b1f208d427167da1
Naww137 <57197000+Naww137@users.noreply.github.com>
2023-10-19 15:29:50 -0400

    allexpcap_1sg.inp update to Yield

--- Fitting/noah_dev/gen_data.py
+++ Fitting/noah_dev/gen_data.py
@@ -24,26 +24,33 @@ l_max = 1       # highest order l-wave to consider
 
 
 
-experiment_parameters = {'bw': {'val':0.1024,    'unc'   :   0}} 
+experiment_parameters = {'bw': {'val':0.0256,    'unc'   :   0}} 
 input_options = {'Add Noise': True,
             'Calculate Covariance': False,
             'Sample TURP': True}
 
-energy_grid = [75, 125]
+energy_grid = [200, 250]
 exp = Experiment(energy_grid, 
                         input_options=input_options, 
                         experiment_parameters=experiment_parameters)
 
-Gg_DOF = 10
-spin_groups = [ (3.0,1,0) ]
-res_par_avg = make_res_par_avg(D_avg = 8.79, 
-                            Gn_avg= 0.658, #0.658, 
+spin_groups = [ (3.0,1,0)]  # ,  (4.0,1,0)]
+res_par_avg_1 = make_res_par_avg(J_ID=1,
+                                 D_avg = 8.79, 
+                            Gn_avg= 46.4, #0.658, 
                             n_dof = 1, 
                             Gg_avg = 64.0, 
-                            g_dof = Gg_DOF, 
+                            g_dof = 1000, 
+                            print = False)
+res_par_avg_2 = make_res_par_avg(J_ID=2,
+                                 D_avg = 4.99, 
+                            Gn_avg= 35.5, #0.658, 
+                            n_dof = 1, 
+                            Gg_avg = 64.0, 
+                            g_dof = 1000, 
                             print = False)
 
-average_parameters = {'3.0':res_par_avg}
+average_parameters = {'3.0':res_par_avg_1} # , '4.0': res_par_avg_2}
 Ta_pair = Particle_Pair( ac, M, m, I, i, l_max,
                                 input_options={},
                                 spin_groups=spin_groups,
@@ -56,7 +63,7 @@ sammyRTO = sammy_classes.SammyRunTimeOptions(
     model = 'XCT',
     reaction = 'total',
     solve_bayes = False,
-    inptemplate= "allexptot_1sg.inp",
+    inptemplate= "allexptot_2sg.inp",
     energy_window = None,
     sammy_runDIR = 'SAMMY_runDIR',
     keep_runDIR = False,
@@ -69,35 +76,23 @@ sammyINP = sammy_classes.SammyInputData(
     energy_grid = np.sort(exp.energy_domain),
     # energy_grid = exp.energy_domain,
     temp = 304.5,
-    FP=35.185,
-    frac_res_FP=0.049600,
-    target_thickness=0.067166)
+    FP=75.0,
+    frac_res_FP=0.025,
+    target_thickness=0.005) #=0.067166
 
 
 
-case_file = '/Users/noahwalton/Documents/GitHub/ATARI/Fitting/noah_dev/fit_w_sammy/data_capcorr.hdf5'  # if using hdf5
+case_file = '/Users/noahwalton/research_local/resonance_fitting/ATARI_workspace/RM_allexp/sammy/data_1.hdf5'  # if using hdf5
 
-dataset_range = (0, 10)
-
-# samples_not_generated = generate(Ta_pair, exp, 
-#                                         'sammy', 
-#                                         dataset_range, 
-#                                         case_file,
-#                                         fixed_resonance_ladder=None, 
-#                                         open_data=None,
-#                                         vary_Erange=None,
-#                                         use_hdf5=True,
-#                                         overwrite = False,
-#                                         sammy_RTO=sammyRTO,
-#                                         sammy_INP=sammyINP
-#                                                                     )
-
-
+dataset_range = (0, 500)
 
 for isample in range(max(dataset_range)):
 
+
+    # sample ladder
     true_resladder = Ta_pair.sample_resonance_ladder(energy_grid, spin_groups, average_parameters)
     sammyINP.resonance_ladder = true_resladder
+
 
     # transmission data generation
     sammyRTO.inptemplate = "allexptot_1sg.inp"
@@ -118,11 +113,12 @@ for isample in range(max(dataset_range)):
     sammyRTO.reaction = "capture"
     out_cap = sammy_functions.run_sammy(sammyINP, sammyRTO)
 
-    unc_scale = 0.1
     cap_pw = copy(out_cap.pw[["E"]])
     cap_pw["true"] = out_cap.pw["theo_xs"]
-    cap_pw["exp"] = abs(np.random.default_rng().normal(out_cap.pw["theo_xs"], np.sqrt(out_cap.pw["theo_xs"])*unc_scale ))
-    cap_pw["exp_unc"] = np.sqrt(cap_pw["exp"])*unc_scale
+    cap_std = (np.sqrt(out_cap.pw["theo_xs"])+1) * 0.01
+    cap_pw["exp"] = abs(np.random.default_rng().normal(out_cap.pw["theo_xs"],  cap_std))
+    cap_pw["exp_unc"] = cap_std
+
 
     # write to file
     h5io.write_par(case_file, isample, true_resladder, 'true')

1116ef423ea670c8e386608d0932d90a3e179ef5
Naww137 <57197000+Naww137@users.noreply.github.com>
2024-02-06 12:22:20 -0500

    autotest script

--- run_tests.py
+++ run_tests.py
@@ -9,25 +9,30 @@ if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     loader = unittest.TestLoader()
 
-    # create test suites
+    ### general ATARI test suites
     parameter_distribution_test_suite = loader.loadTestsFromModule(test_distributions)
     resonance_generator_test_suite = loader.loadTestsFromModule(test_res_generator)
-
-    sammy_test_suite = loader.loadTestsFromModule(test_sammy_interface)
+    # TODO: add theory_module_test_suite
     syndat_test_suite = loader.loadTestsFromModule(test_syndat)
-   
 
+    ### those that require sammy
+    sammy_test_suite = loader.loadTestsFromModule(test_sammy_interface)
+    # TODO: add fitting_from_theory_test_suite
+    # TODO: add autofit_test_suite
+
+   
     print("Running parameter distribution test suite")
     result = runner.run(parameter_distribution_test_suite)
 
     print("Running resonance generator test suite")
     result = runner.run(resonance_generator_test_suite)
 
-    print("Running SAMMY test suite")
-    result = runner.run(sammy_test_suite)
-
     print("Running Syndat test suite")
     result = runner.run(syndat_test_suite)
+
+    print("Now running tests that require SAMMY")
+    print("Running sammy_interface test suite")
+    result = runner.run(sammy_test_suite)
 
 
     

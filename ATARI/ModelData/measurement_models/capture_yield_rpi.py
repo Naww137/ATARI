big fix for saving covariance data

ATARI/ModelData/measurement_models/capture_yield_rpi.py
@@ -30,18 +30,18 @@ def get_covT():
 def reduce_raw_count_data(raw_data, model_parameters):
 
     ### counts to count rates for target gamma and background measurements
-    crg, dcrg = cts_to_ctr(raw_data.cg, raw_data.dcg, model_parameters.incident_neutron_spectrum_f.bw, model_parameters.trig_g[0])
-    brg, dbrg = cts_to_ctr(model_parameters.background_spectrum_bg.c, 
-                           model_parameters.background_spectrum_bg.dc,
+    crg, dcrg = cts_to_ctr(raw_data.ctg, raw_data.dctg, model_parameters.incident_neutron_spectrum_f.bw, model_parameters.trig_g[0])
+    brg, dbrg = cts_to_ctr(model_parameters.background_spectrum_bg.ct, 
+                           model_parameters.background_spectrum_bg.dct,
                            model_parameters.incident_neutron_spectrum_f.bw, 
                            model_parameters.trig_bg[0])
     ### counts to count rates for incident flux measurement
-    cr_flux, dcr_flux = cts_to_ctr(model_parameters.incident_neutron_spectrum_f.c,
-                         model_parameters.incident_neutron_spectrum_f.dc,
+    cr_flux, dcr_flux = cts_to_ctr(model_parameters.incident_neutron_spectrum_f.ct,
+                         model_parameters.incident_neutron_spectrum_f.dct,
                          model_parameters.incident_neutron_spectrum_f.bw,
                          model_parameters.trig_f[0])
-    br_flux, dbr_flux = cts_to_ctr(model_parameters.background_spectrum_bf.c,
-                         model_parameters.background_spectrum_bf.dc,
+    br_flux, dbr_flux = cts_to_ctr(model_parameters.background_spectrum_bf.ct,
+                         model_parameters.background_spectrum_bf.dct,
                          model_parameters.incident_neutron_spectrum_f.bw,
                          model_parameters.trig_bf[0])
 
@@ -64,6 +64,8 @@ def reduce_raw_count_data(raw_data, model_parameters):
     # diag_stat = None
     # diag_sys = None
     # data =[diag_stat, diag_sys, Jac_sys, Cov_sys]
+    if np.any(Yield==0):
+        pass
 
     return Yield, Yield_uncertainty
 
@@ -75,20 +77,20 @@ def inverse_reduction(pw_true, true_model_parameters):
     #                       'dc'   :   np.ones(len(pw_true.E))*TURP.Scaling[1]})
     
     ### relative flux rate measurement
-    cr_flux, dcr_flux = cts_to_ctr(true_model_parameters.incident_neutron_spectrum_f.c,
-                                true_model_parameters.incident_neutron_spectrum_f.dc,
+    cr_flux, dcr_flux = cts_to_ctr(true_model_parameters.incident_neutron_spectrum_f.ct,
+                                true_model_parameters.incident_neutron_spectrum_f.dct,
                                 true_model_parameters.incident_neutron_spectrum_f.bw,
                                 true_model_parameters.trig_f[0])
-    br_flux, dbr_flux = cts_to_ctr(true_model_parameters.background_spectrum_bf.c,
-                                true_model_parameters.background_spectrum_bf.dc,
+    br_flux, dbr_flux = cts_to_ctr(true_model_parameters.background_spectrum_bf.ct,
+                                true_model_parameters.background_spectrum_bf.dct,
                                 true_model_parameters.incident_neutron_spectrum_f.bw,
                                 true_model_parameters.trig_bf[0])
 
     relative_flux_rate = cr_flux - br_flux # need to normalize by yield here!
     
     ### target gamma background and count rate
-    br_gamma, dbr_gamma = cts_to_ctr(true_model_parameters.background_spectrum_bg.c,
-                                    true_model_parameters.background_spectrum_bg.dc,
+    br_gamma, dbr_gamma = cts_to_ctr(true_model_parameters.background_spectrum_bg.ct,
+                                    true_model_parameters.background_spectrum_bg.dct,
                                     true_model_parameters.incident_neutron_spectrum_f.bw,
                                     true_model_parameters.trig_bg[0])
     cr_gamma_true = np.multiply(pw_true.true, relative_flux_rate)/true_model_parameters.fn[0] + br_gamma
@@ -130,10 +132,10 @@ class capture_yield_rpi_parameters:
 
     def __init__(self, **kwargs):
 
-        self.trig_g     =  (10000000,   0)
-        self.trig_bg    =  (1000000,  0)
-        self.trig_f     =  (10000000,   0)
-        self.trig_bf    =  (1000000,  0)
+        self.trig_g     =  (12000000,   0)
+        self.trig_bg    =  (10000000,  0)
+        self.trig_f     =  (12000000,   0)
+        self.trig_bf    =  (10000000,  0)
         self.fn         =  (1,          0)
     
         self.background_spectrum_bg = None
@@ -159,10 +161,10 @@ class capture_yield_rpi_parameters:
                         sample = np.random.normal(loc=mean, scale=uncertainty)
                     sampled_params[param_name] = (sample, 0.0)
                 if isinstance(param_values, pd.DataFrame):
-                    new_c = np.random.poisson(param_values.c)#, scale=param_values.dc)
+                    new_c = np.random.poisson(param_values.ct)#, scale=param_values.dc)
                     df = deepcopy(param_values)
-                    df.loc[:,'c'] = new_c
-                    df.loc[:,'dc'] = np.sqrt(new_c)
+                    df.loc[:,'ct'] = new_c
+                    df.loc[:,'dct'] = np.sqrt(new_c)
                     sampled_params[param_name] = df
 
         return capture_yield_rpi_parameters(**sampled_params)
@@ -208,7 +210,12 @@ class Capture_Yield_RPI:
         return self.model_parameters.sample_parameters(true_model_parameters)
 
 
-    def approximate_unknown_data(self, exp_model, smooth):
+    def approximate_unknown_data(self, exp_model, smooth, check_trig=False):
+        
+        if check_trig:
+            for each in [self.model_parameters.trig_g, self.model_parameters.trig_bg, self.model_parameters.trig_f, self.model_parameters.trig_bf]:
+                if exp_model.channel_widths['chw'][0]*1e-9 * each[0] < 1:
+                    print("WARNING: the linac trigers in you generative measurement model are not large enough for the channel bin width. This will result in many bins with 0 counts")
 
         if self.model_parameters.background_spectrum_bg is None:
             background_spectrum_bg = approximate_gamma_background_spectrum(exp_model.energy_grid, 
@@ -305,11 +312,12 @@ class Capture_Yield_RPI:
         else:
             c = true_gamma_counts
         c[c==0] = 1
-        assert(all(c> 0))
+        # assert(all(c> 0))
         dc = np.sqrt(c)
 
-        raw_data.loc[:, 'cg'] = c
-        raw_data.loc[:, 'dcg'] = dc
+        raw_data.loc[:, 'ctg_true'] = true_gamma_counts
+        raw_data.loc[:, 'ctg'] = c
+        raw_data.loc[:, 'dctg'] = dc
         
         # count_rate=pd.DataFrame({'c'    :   count_rate,
         #                          'dc'   :   count_rate_uncertainty})
@@ -385,18 +393,19 @@ class Capture_Yield_RPI:
             CovY = pd.DataFrame(CovY, columns=Yg.E, index=Yg.E)
             CovY.index.name = None
             Yg['exp_unc'] = np.sqrt(np.diag(CovY))
-
+            covariance_data = {}
             if options.explicit_covariance:
-                self.covariance_data['CovY'] = CovY
+                covariance_data['CovY'] = CovY
             else:
                 raise ValueError("not implemented")
 
         else:
             diag_tot = unc_data
             Yg.loc[:,'exp_unc'] = diag_tot
+            covariance_data = {}
 
         ## fix for zero gamma counts
         # Yg = Yg.loc[Yg.exp!=0]
-        assert(np.all(Yg.exp!=0))
+        # assert(np.all(Yg.exp!=0))
 
-        return Yg
+        return Yg, covariance_data, raw_data

ATARI/ModelData/measurement_models/transmission_rpi.py
@@ -233,14 +233,14 @@ def inverse_reduction(sample_df, open_df, add_noise, sample_turp, trigo,trigs, k
         Dataframe containing data for sample out.
     """
     # calculate open count rates
-    Cr, dCr = cts_to_ctr(open_df.c, open_df.dc, open_df.bw, trigo) # cts_o/(bw*trig)
-    open_df.loc[:,'O_cps'] = Cr; 
-    open_df.loc[:,'O_dcps'] = dCr
+    Cr, dCr = cts_to_ctr(open_df.ct, open_df.dct, open_df.bw, trigo) # cts_o/(bw*trig)
+    open_df.loc[:,'c'] = Cr; 
+    open_df.loc[:,'dc'] = dCr
     
     # calculate sample in count rate from theoretical transmission, bkg, m,k, and open count rate
     [m1,m2,m3,m4] = alpha
     cr = (sample_df["true"]*(m3*Cr - m4*K*Bi - B0) + m2*k*Bi + b0)/m1
-    sample_df.loc[:,'cps_true'] = cr
+    sample_df.loc[:,'c_true'] = cr
 
     # calculate expected sample in counts from count rate
     c = cr*open_df.bw*trigs
@@ -271,9 +271,10 @@ def inverse_reduction(sample_df, open_df, add_noise, sample_turp, trigo,trigs, k
     assert(c.all() >= 0)
     dc = np.sqrt(c)
     
-    sample_df.loc[:,'c'] = c
-    sample_df.loc[:,'dc'] = dc
-    sample_df.loc[:,'c_true'] = theo_c
+    sample_df.loc[:,'cts'] = c
+    sample_df.loc[:,'dcts'] = dc
+    sample_df.loc[:,'cts_true'] = theo_c
+    sample_df.loc[:, 'bw'] = open_df.bw
 
     return sample_df, open_df
 
@@ -343,10 +344,10 @@ class transmission_rpi_parameters:
                             sample = np.random.normal(loc=mean, scale=uncertainty)
                     sampled_params[param_name] = (sample, 0.0)
                 if isinstance(param_values, pd.DataFrame):
-                    new_c = np.random.normal(loc=param_values.c, scale=param_values.dc)
+                    new_c = np.random.normal(loc=param_values.ct, scale=param_values.dct)
                     df = deepcopy(param_values)
-                    df.loc[:,'c'] = new_c
-                    df.loc[:,'dc'] = np.sqrt(new_c)
+                    df.loc[:,'ct'] = new_c
+                    df.loc[:,'dct'] = np.sqrt(new_c)
                     sampled_params[param_name] = df
 
         return transmission_rpi_parameters(**sampled_params)
@@ -408,7 +409,7 @@ class Transmission_RPI:
         return self.model_parameters.sample_parameters(true_model_parameters)
     
 
-    def approximate_unknown_data(self, exp_model, smooth):
+    def approximate_unknown_data(self, exp_model, smooth, check_trig = False):
         if self.model_parameters.open_neutron_spectrum is None:
             open_neutron_spectrum = approximate_neutron_spectrum_Li6det(exp_model.energy_grid, 
                                                                     smooth, #self.options.smoothTNCS, 
@@ -443,7 +444,6 @@ class Transmission_RPI:
 
         monitor_array = [true_model_parameters.m1[0], true_model_parameters.m2[0], true_model_parameters.m3[0], true_model_parameters.m4[0]]
         true_Bi = neutron_background_function(true_model_parameters.open_neutron_spectrum.tof*1e-3, true_model_parameters.a_b[0][0], true_model_parameters.a_b[0][1])
-        true_model_parameters.Bi = true_Bi
 
         sample_data, open_data = inverse_reduction(copy(pw_true) ,
                                              true_model_parameters.open_neutron_spectrum ,
@@ -458,39 +458,43 @@ class Transmission_RPI:
                                              true_model_parameters.b0o[0],
                                              monitor_array, true_model_parameters.m1[1])
         
-        # open_data.rename(columns={"c":"O_c", "dc":"O_dc","cps":"O_cps", "dcps":"O_dcps"}, inplace=True)
+        # open_data.rename(columns={"c":"cto", "dc":"dcto","cps":"c", "dcps":"dc"}, inplace=True)
         # open_data = open_data.round({'E':6})
         # sample_data = sample_data.round({'E':6})
         # pw_raw = open_data.merge(sample_data, how='left', on=['tof','E'])
         # pw_raw["bkg_true"] = 
 
-        return sample_data
+        raw_data = copy(sample_data)
+
+        raw_data['bs_true'] = true_model_parameters.m2[0]*true_Bi*true_model_parameters.ks[0] + true_model_parameters.b0s[0]
+        raw_data['bo_true'] = true_model_parameters.m4[0]*true_Bi*true_model_parameters.ko[0] + true_model_parameters.b0o[0]
+
+        raw_data['cto_true'] = open_data['ct']
+        raw_data['dcto_true'] = open_data['dct']
+        raw_data['co_true'] = open_data['c']
+        raw_data['dco_true'] = open_data['dc']
+
+        return raw_data
 
 
 
-    def reduce_raw_data(self, raw_data, 
-                        # neutron_spectrum, 
+    def reduce_raw_data(self, 
+                        raw_data,  
                         options):
         """
         Reduces the raw count data (sample in/out) to Transmission data and propagates uncertainty.
 
         """
         assert isinstance(self.model_parameters.open_neutron_spectrum, pd.DataFrame)
-        assert(np.isclose(np.sum(abs(self.model_parameters.open_neutron_spectrum.tof.values - raw_data.tof.values)), 0,atol=1e-3))
+        assert(np.isclose(max(abs(self.model_parameters.open_neutron_spectrum.tof.values - raw_data.tof.values)/raw_data.tof.values), 0,atol=1e-7))
 
-        # Code for re-binning data
-        # if self.gfactors is not None:
-        #     # Re-bin the data according to new structure
-        #     grouped_odat = exp_effects.regroup(self.odat.tof, self.odat.c, self.gfactors, self.cpts)
-        #     grouped_sdat = exp_effects.regroup(self.sdat.tof, self.sdat.c, self.gfactors, self.cpts)
-        #     odat = pd.DataFrame(grouped_odat, columns=['tof','bw','c','dc'])
-        #     sdat = pd.DataFrame(grouped_sdat, columns=['tof','bw','c','dc'])
-
-        #     # calculate energy and redefine experiment.odat/sdat with the regrouped data
-        #     odat['E'] = exp_effects.t_to_e((odat.tof-self.redpar.val.t0)*1e-6, self.redpar.val.FP, True)
-        #     sdat['E'] = exp_effects.t_to_e((odat.tof-self.redpar.val.t0)*1e-6, self.redpar.val.FP, True)
-        #     self.odat = odat
-        #     self.sdat = sdat
+        ### complete raw dataframe with count rates using reductive model parameters
+        cr, dcr = cts_to_ctr(raw_data.cts, raw_data.dcts, raw_data.bw, self.model_parameters.trigs[0])
+        raw_data['co']  = cr
+        raw_data['dco'] = dcr
+        Cr, dCr = cts_to_ctr(self.model_parameters.open_neutron_spectrum.ct, self.model_parameters.open_neutron_spectrum.dct, self.model_parameters.open_neutron_spectrum.bw, self.model_parameters.trigo[0])
+        raw_data['cs']  = Cr
+        raw_data['dcs'] = dCr
 
         # create transmission object
         trans = pd.DataFrame()
@@ -498,13 +502,10 @@ class Transmission_RPI:
         trans['E'] = raw_data.E
         trans['true'] = raw_data.true
 
-        # get count rates for sample in data
-        # self.sdat['cps'], self.sdat['dcps'] = exp_effects.cts_to_ctr(self.sdat.c, self.sdat.dc, self.sdat.bw*1e-6, self.redpar.val.trigs)
-        # self.odat['cps'], self.odat['dcps'] = exp_effects.cts_to_ctr(self.odat.c, self.odat.dc, self.odat.bw*1e-6, self.redpar.val.trigs)
-
         # estimated background function
         Bi = neutron_background_function(self.model_parameters.open_neutron_spectrum.tof*1e-3, self.model_parameters.a_b[0][0], self.model_parameters.a_b[0][1])
-        self.model_parameters.Bi = Bi
+        raw_data['bs'] = self.model_parameters.m2[0]*Bi*self.model_parameters.ks[0] + self.model_parameters.b0s[0]
+        raw_data['bo'] = self.model_parameters.m4[0]*Bi*self.model_parameters.ko[0] + self.model_parameters.b0o[0]
 
         # define systematic uncertainties
         # sys_unc = self.redpar.unc[['a','b','ks','ko','b0s','b0o','m1','m2','m3','m4']].astype(float)
@@ -513,10 +514,10 @@ class Transmission_RPI:
         # monitor_array = [self.redpar.val.m1, self.redpar.val.m2, self.redpar.val.m3, self.redpar.val.m4]
         monitor_array = [self.model_parameters.m1[0], self.model_parameters.m2[0], self.model_parameters.m3[0], self.model_parameters.m4[0]]
         trans.loc[:,'exp'], unc_data, rates = reduce_raw_count_data(raw_data.tof.values,
-                                                              raw_data.c.values, 
-                                                              self.model_parameters.open_neutron_spectrum.c.values,
-                                                              raw_data.dc.values, 
-                                                              self.model_parameters.open_neutron_spectrum.dc.values,
+                                                              raw_data.cts.values, 
+                                                              self.model_parameters.open_neutron_spectrum.ct.values,
+                                                              raw_data.dcts.values, 
+                                                              self.model_parameters.open_neutron_spectrum.dct.values,
                                                               self.model_parameters.open_neutron_spectrum.bw.values,
                                                               self.model_parameters.trigo[0],
                                                               self.model_parameters.trigs[0],
@@ -541,22 +542,24 @@ class Transmission_RPI:
             CovT = pd.DataFrame(CovT, columns=trans.E, index=trans.E)
             CovT.index.name = None
             trans['exp_unc'] = np.sqrt(np.diag(CovT))
-
+            covariance_data = {}
+        
             if options.explicit_covariance:
-                self.covariance_data['CovT'] = CovT
+                covariance_data['CovT'] = CovT
             else:
                 pass
 
-            self.covariance_data['Cov_sys'] = Cov_sys
+            covariance_data['Cov_sys'] = Cov_sys
             Jac_sys = pd.DataFrame(Jac_sys, columns=trans.E)
-            self.covariance_data['Jac_sys'] = Jac_sys
-            self.covariance_data["diag_stat"] = pd.DataFrame({'var_stat':diag_stat}, index=trans.E)
+            covariance_data['Jac_sys'] = Jac_sys
+            covariance_data["diag_stat"] = pd.DataFrame({'var_stat':diag_stat}, index=trans.E)
         
         
         else:
             trans['exp_unc'] = np.sqrt(diag_stat + diag_sys)
+            covariance_data = {}
 
-        return trans
+        return trans, covariance_data, raw_data
 
 
 

ATARI/sammy_interface/sammy_functions.py
@@ -428,17 +428,17 @@ def write_saminp(filepath,
                     f.write(f'{cmd}\n')
             
             elif line.startswith("%%%card2%%%"):
-                f.write(f"{particle_pair.isotope: <9} {np.round(particle_pair.M,6): <9} {float(min(experimental_model.energy_range)): <9} {float(max(experimental_model.energy_range)): <9}      {rto.options['iterations']: <5} \n")
+                f.write(f"{particle_pair.isotope: <9} {np.round(particle_pair.M,5): <9} {np.round(float(min(experimental_model.energy_range)),3)-0.001: <9} {np.round(float(max(experimental_model.energy_range)),3)+0.001: <9}      {rto.options['iterations']: <5} \n")
 
 
             elif line.startswith('%%%card5/6%%%'):
                 if broadening:
-                    f.write(f'  {float(experimental_model.temp[0]):<8}  {float(experimental_model.FP[0]):<8}  {float(experimental_model.FP[1]):<8}        \n')
+                    f.write(f'  {np.round(float(experimental_model.temp[0]),4):<8}  {float(experimental_model.FP[0]):<8}  {float(experimental_model.FP[1]):<8}        \n')
                 else:
                     pass
 
             elif line.startswith('%%%card7%%%'): #ac*10 because sqrt(bn) -> fm for sammy 
-                f.write(f'  {round(float(particle_pair.ac)*10, 7):<8}  {float(experimental_model.n[0]):<8}                       0.00000          \n')
+                f.write(f'  {np.round(float(particle_pair.ac)*10, 7):<8}  {np.round(float(experimental_model.n[0]),6):<8}                       0.00000          \n')
 
             elif line.startswith('%%%card8%%%'):
                 f.write(f'{reaction}\n')
@@ -578,7 +578,7 @@ def get_endf_parameters(endf_file, matnum, sammyRTO: SammyRunTimeOptions):
     runsammy_process = subprocess.run(
                                     [f"sh", "-c", f"{sammyRTO.path_to_SAMMY_exe}<pipe.sh"], 
                                     cwd=os.path.realpath(sammyRTO.sammy_runDIR),
-                                    capture_output=True, timeout=60*2
+                                    capture_output=True, timeout=60*10
                                     )
 
     resonance_ladder = readpar(os.path.join(sammyRTO.sammy_runDIR, "SAMNDF.PAR"))
@@ -731,6 +731,8 @@ def run_sammy(sammyINP: SammyInputData, sammyRTO:SammyRunTimeOptions):
         sammy_OUT.par_post = par_df
         sammy_OUT.chi2_post = chi2
         sammy_OUT.chi2n_post = chi2n
+        sammy_OUT.chi2 = None
+        sammy_OUT.chi2n = None
 
         if sammyRTO.get_ECSCM:
             est_df, ecscm = get_ECSCM(sammyRTO, sammyINP)
@@ -952,7 +954,7 @@ def setup_YW_scheme(sammyRTO, sammyINPyw):
 def iterate_for_nonlin_and_update_step_par(iterations, step, rundir):
     runsammy_bay0 = subprocess.run(
                             ["sh", "-c", f"./BAY0.sh {step}"], cwd=os.path.realpath(rundir),
-                            capture_output=True, timeout=60*5
+                            capture_output=True, timeout=60*10
                             )
 
     for i in range(1, iterations+1):
@@ -963,7 +965,7 @@ def iterate_for_nonlin_and_update_step_par(iterations, step, rundir):
 
         runsammy_bay0 = subprocess.run(
                                     ["sh", "-c", f"./BAYiter.sh {i}"], cwd=os.path.realpath(rundir),
-                                    capture_output=True, timeout=60*5
+                                    capture_output=True, timeout=60*10
                                     )
 
     # Move par file from final iteration 
@@ -1074,7 +1076,7 @@ def step_until_convergence_YW(sammyRTO, sammyINPyw):
 
 def plot_YW(sammyRTO, dataset_titles, i):
     out = subprocess.run(["sh", "-c", f"./plot.sh {i}"], 
-                cwd=os.path.realpath(sammyRTO.sammy_runDIR), capture_output=True, text=True, timeout=60*5
+                cwd=os.path.realpath(sammyRTO.sammy_runDIR), capture_output=True, text=True, timeout=60*10
                         )
     par = readpar(os.path.join(sammyRTO.sammy_runDIR,f"results/step{i}.par"))
     lsts = []

tests/test_syndat.py
@@ -28,19 +28,21 @@ class TestTransmissionRPIModel(unittest.TestCase):
     def setUpClass(cls):
         cls.pair = Particle_Pair()
 
+        cls.print_out = False
+
         ## parameters to reduce default uncertainties to be in linear regime
         # but don't eliminate covariance because we want to test it 
         cls.model_par = {
-        'trigs' :   (1e12,  0),
-        'trigo' :   (1e12,  0),
-        'm1'    :   (1,     0.00016) ,#0.016
-        'm2'    :   (1,     0.00008) ,#0.008
-        'm3'    :   (1,     0.00018) ,#0.018
-        'm4'    :   (1,     0.00005) ,#0.005
-        'ks'    :   (0.563, 0.000240), #0.02402339737495515
-        'ko'    :   (1.471, 0.000557), #0.05576763648617445
-        'b0s'   :   (9.9,   0.001) ,#0.1
-        'b0o'   :   (13.4,  0.007) ,#0.7
+        'trigs' :   (1e10,  0),
+        'trigo' :   (1e10,  0),
+        # 'm1'    :   (1,     0.0016) ,#0.016
+        # 'm2'    :   (1,     0.0008) ,#0.008
+        # 'm3'    :   (1,     0.0018) ,#0.018
+        # 'm4'    :   (1,     0.0005) ,#0.005
+        # 'ks'    :   (0.563, 0.00240), #0.02402339737495515
+        # 'ko'    :   (1.471, 0.00557), #0.05576763648617445
+        # 'b0s'   :   (9.9,   0.01) ,#0.1
+        # 'b0o'   :   (13.4,  0.07) ,#0.7
         }
 
 
@@ -54,32 +56,27 @@ class TestTransmissionRPIModel(unittest.TestCase):
         self.assertAlmostEqual(residual, 0, places=10)
 
 
-    # def test_random_energy(self):
+    def test_random_energy(self):
 
-    #     energy_grid = np.sort(np.random.default_rng().uniform(10,3000,10)) #np.linspace(min(energy_range),max(energy_range),10) # energy below 10 has very low counts due to approximate open spectrum
-    #     df_true = pd.DataFrame({'E':energy_grid, 'true':np.random.default_rng().uniform(0.7,1.0,10)})
-    #     df_true.sort_values('E', ascending=True, inplace=True)
+        energy_grid = np.sort(np.random.default_rng().uniform(10,3000,10)) #np.linspace(min(energy_range),max(energy_range),10) # energy below 10 has very low counts due to approximate open spectrum
+        df_true = pd.DataFrame({'E':energy_grid, 'true':np.random.default_rng().uniform(0.01,1.0,10)})
+        df_true.sort_values('E', ascending=True, inplace=True)
 
-    #     generative_model = Transmission_RPI(**self.model_par)
-    #     reductive_model = Transmission_RPI(**self.model_par)
+        generative_model = Transmission_RPI(**self.model_par)
+        reductive_model = Transmission_RPI(**self.model_par)
 
-    #     synOPT = syndatOPT(sampleRES=False, calculate_covariance=True, explicit_covariance=True, sampleTMP=True, smoothTNCS=True) 
-    #     # exp_model = Experimental_Model(energy_grid=energy_grid, energy_range = [min(energy_grid), max(energy_grid)])
-    #     SynMod = Syndat_Model(generative_experimental_model=None, generative_measurement_model=generative_model, reductive_measurement_model=reductive_model, options=synOPT)
-        
-    #     # reset energy grid, and open neutron spectrum
-    #     SynMod.generative_experimental_model.energy_grid = energy_grid
-    #     SynMod.generative_experimental_model.energy_range = [10,3000]
-    #     SynMod.generative_measurement_model.model_parameters.open_neutron_spectrum = None
-    #     SynMod.reductive_measurement_model.model_parameters.open_neutron_spectrum = None
+        synOPT = syndatOPT(sampleRES=False, calculate_covariance=True, explicit_covariance=True, sampleTMP=True, smoothTNCS=True) 
+        exp_model = Experimental_Model(energy_grid=energy_grid, energy_range = [min(energy_grid), max(energy_grid)])
+        SynMod = Syndat_Model(generative_experimental_model=exp_model, generative_measurement_model=generative_model, reductive_measurement_model=reductive_model, options=synOPT)
 
-    #     mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=500, print_out=True) #noise_distribution_test(SynMod, print_out=True)
-    #     self.assertTrue( np.isclose(mean_of_residual, 0, atol=1e-1), 
-    #                     "Mean of residuals is not 0")
-    #     self.assertTrue( norm_test_on_residual.pvalue>1e-5, 
-    #                     "Normalized residuals are not standard normal")
-    #     self.assertTrue( kstest_on_chi2.pvalue>1e-5, 
-    #                     "Chi2 of data does not have appropriate DOF")
+
+        mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=250, print_out=self.print_out) #noise_distribution_test(SynMod, print_out=True)
+        self.assertTrue( np.isclose(mean_of_residual, 0, atol=1e-1), 
+                        "Mean of residuals is not 0")
+        self.assertTrue( norm_test_on_residual.pvalue>1e-5, 
+                        "Normalized residuals are not standard normal")
+        self.assertTrue( kstest_on_chi2.pvalue>1e-5, 
+                        "Chi2 of data does not have appropriate DOF")
         
 
     def test_default_energy_window(self):
@@ -93,8 +90,8 @@ class TestTransmissionRPIModel(unittest.TestCase):
         synOPT = syndatOPT(sampleRES=False, calculate_covariance=True, explicit_covariance=True, sampleTMP=True, smoothTNCS=True) 
         SynMod = Syndat_Model(generative_experimental_model=exp_model, generative_measurement_model=generative_model, reductive_measurement_model=reductive_model, options=synOPT)
         
-        mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=250, print_out=True) #noise_distribution_test(SynMod, print_out=True)
-        self.assertTrue( np.isclose(mean_of_residual, 0, atol=1e-2), 
+        mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=250, print_out=self.print_out) #noise_distribution_test(SynMod, print_out=True)
+        self.assertTrue( np.isclose(mean_of_residual, 0, atol=1e-1), 
                         "Mean of residuals is not 0")
         self.assertTrue( norm_test_on_residual.pvalue>1e-5, 
                         "Normalized residuals are not standard normal")
@@ -105,13 +102,13 @@ class TestTransmissionRPIModel(unittest.TestCase):
     def test_with_given_TNCS(self):
 
         exp_model = Experimental_Model(channel_widths={"maxE": [3000],"chw": [1200.0],"dchw": [0.8]})
-        df_true = pd.DataFrame({'E': exp_model.energy_grid, 'tof':exp_model.tof_grid,'true': np.random.default_rng().uniform(0.7,1.0,len(exp_model.energy_grid)) })#np.ones(len(exp_model.energy_grid))*0.9 })
+        df_true = pd.DataFrame({'E': exp_model.energy_grid, 'tof':exp_model.tof_grid,'true': np.random.default_rng().uniform(0.1,1.0,len(exp_model.energy_grid)) })#np.ones(len(exp_model.energy_grid))*0.9 })
 
         open_df = pd.DataFrame({"E":exp_model.energy_grid, 
                                 'tof': exp_model.tof_grid,
                                 'bw': abs(np.append(np.diff(exp_model.tof_grid), np.diff(exp_model.tof_grid)[-1])*1e-9),
-                               'c':np.ones(len(exp_model.energy_grid))*1e9,
-                               'dc': np.ones(len(exp_model.energy_grid))*np.sqrt(1e9)})
+                               'c':np.ones(len(exp_model.energy_grid))*1e10,
+                               'dc': np.ones(len(exp_model.energy_grid))*np.sqrt(1e10)})
         # open_df = approximate_neutron_spectrum_Li6det(exp_model.energy_grid, True, exp_model.FP[0], exp_model.t0[0], self.model_par['trigo'][0])
 
         generative_model = Transmission_RPI(**self.model_par, open_neutron_spectrum=open_df)
@@ -120,15 +117,65 @@ class TestTransmissionRPIModel(unittest.TestCase):
         synOPT = syndatOPT(sampleRES=False, calculate_covariance=True, explicit_covariance=True, sampleTMP=True, smoothTNCS=True) 
         SynMod = Syndat_Model(generative_experimental_model=exp_model, generative_measurement_model=generative_model, reductive_measurement_model=reductive_model, options=synOPT)
         
-        mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=250, print_out=True) #noise_distribution_test(SynMod, print_out=True)
-        self.assertTrue( np.isclose(mean_of_residual, 0, atol=1e-2), 
+        mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=250, print_out=self.print_out) #noise_distribution_test(SynMod, print_out=True)
+        self.assertTrue( np.isclose(mean_of_residual, 0, atol=1e-1), 
                         "Mean of residuals is not 0")
         self.assertTrue( norm_test_on_residual.pvalue>1e-5, 
-                        "Normalized residuals are not standard normal")
+                        f"Normalized residuals are not standard normal: pvalue = {norm_test_on_residual.pvalue}")
         self.assertTrue( kstest_on_chi2.pvalue>1e-5, 
                         "Chi2 of data does not have appropriate DOF")
 
+
+
+    def test_covariance(self):
+
+        exp_model = Experimental_Model(channel_widths={"maxE": [3000],"chw": [1200.0],"dchw": [0.8]})
+        df_true = pd.DataFrame({'E': exp_model.energy_grid, 'tof':exp_model.tof_grid,'true': np.random.default_rng().uniform(0.1,1.0,len(exp_model.energy_grid)) })#np.ones(len(exp_model.energy_grid))*0.9 })
+
+        generative_model = Transmission_RPI(**self.model_par)
+        reductive_model = Transmission_RPI(**self.model_par)
+
+        synOPT = syndatOPT(sampleRES=False, calculate_covariance=True, explicit_covariance=True, sampleTMP=True, smoothTNCS=True) 
+        SynMod = Syndat_Model(generative_experimental_model=exp_model, generative_measurement_model=generative_model, reductive_measurement_model=reductive_model, options=synOPT)
+
+        SynMod.sample(pw_true=df_true, num_samples=5)
+        s1 = SynMod.samples[0]
+        s5 = SynMod.samples[4]
+
+        self.assertTrue(all(np.isclose(np.diag(s1.covariance_data['CovT']), s1.pw_reduced.exp_unc**2, atol=1e-5)))
+        self.assertTrue(all(np.isclose(np.diag(s5.covariance_data['CovT']), s5.pw_reduced.exp_unc**2, atol=1e-5)))
+
+        self.assertTrue(np.all(np.isclose(np.diag(s1.covariance_data['diag_stat']) + s1.covariance_data['Jac_sys'].values.T @ s1.covariance_data['Cov_sys'] @ s1.covariance_data['Jac_sys'].values, s1.covariance_data['CovT'], atol=1e-5)))
+        self.assertTrue(np.all(np.isclose(np.diag(s5.covariance_data['diag_stat']) + s5.covariance_data['Jac_sys'].values.T @ s5.covariance_data['Cov_sys'] @ s5.covariance_data['Jac_sys'].values, s5.covariance_data['CovT'], atol=1e-5)))
         
+        self.assertFalse(any([np.all(s1.covariance_data[key] == s5.covariance_data[key]) for key in s1.covariance_data.keys() if key not in ['Cov_sys']]))
+
+
+    # def test_sample_saving(self, sammy_path):
+    #     self.pair.add_spin_group(Jpi='3.0', J_ID=1, D=9.0030, gn2_avg=452.56615, gn2_dof=1, gg2_avg=32.0, gg2_dof=1000)
+
+    #     exp_model = Experimental_Model(channel_widths={"maxE": [3000],"chw": [1200.0],"dchw": [0.8]})
+    #     df_true = pd.DataFrame({'E': exp_model.energy_grid, 'tof':exp_model.tof_grid,'true': np.random.default_rng().uniform(0.1,1.0,len(exp_model.energy_grid)) })#np.ones(len(exp_model.energy_grid))*0.9 })
+
+    #     generative_model = Transmission_RPI(**self.model_par)
+    #     reductive_model = Transmission_RPI(**self.model_par)
+
+    #     synOPT = syndatOPT(sampleRES=True, calculate_covariance=True, explicit_covariance=True, sampleTMP=True, smoothTNCS=True) 
+    #     SynMod = Syndat_Model(generative_experimental_model=exp_model, generative_measurement_model=generative_model, reductive_measurement_model=reductive_model, options=synOPT)
+
+    #     SynMod.sample(pw_true=df_true, num_samples=5)
+    #     s1 = SynMod.samples[0]
+    #     s5 = SynMod.samples[4]
+
+                
+    #     self.assertTrue(all(s1.pw_reduced.tof == s5.pw_reduced.tof))
+    #     self.assertFalse(all(s1.pw_reduced.exp_unc == s5.pw_reduced.exp_unc))
+    #     self.assertFalse(all(s1.pw_reduced.exp == s5.pw_reduced.exp))
+
+    #     self.assertTrue([np.all(np.isclose(s1.covariance_data[key], s5.covariance_data[key], atol=1e-5)) for key in s1.covariance_data.keys()])
+
+
+
 
 
 class TestYieldRPIModel(unittest.TestCase):
@@ -137,11 +184,13 @@ class TestYieldRPIModel(unittest.TestCase):
     def setUpClass(cls):
         cls.pair = Particle_Pair()
 
+        cls.print_out = False
+
         ## parameters to reduce default uncertainties to be in linear regime
         # but don't eliminate covariance because we want to test it 
         cls.model_par = {
-        'trigs' :   (1e12,  0),
-        'trigo' :   (1e12,  0),
+        'trigs' :   (1e7,  0),
+        'trigo' :   (1e7,  0),
         }
 
 
@@ -164,7 +213,7 @@ class TestYieldRPIModel(unittest.TestCase):
 
         synOPT = syndatOPT(smoothTNCS=True, sampleRES=False, calculate_covariance=True, explicit_covariance=True, sampleTMP=True) 
         SynMod = Syndat_Model(generative_experimental_model=exp_model, generative_measurement_model=generative_model, reductive_measurement_model=reductive_model, options=synOPT)
-        mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=250, print_out=True) 
+        mean_of_residual, norm_test_on_residual, kstest_on_chi2 = noise_distribution_test2(SynMod, df_true = df_true, ipert=250, print_out=self.print_out) 
 
         self.assertTrue( np.isclose(mean_of_residual, 0, atol=1e-1), 
                         "Mean of residuals is not 0")
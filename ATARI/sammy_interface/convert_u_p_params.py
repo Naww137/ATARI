Bug fixing sammy derivatives code.

ATARI/sammy_interface/convert_u_p_params.py
@@ -13,11 +13,11 @@ def u2p_E(u_E):
 def p2u_E(p_E):
     return np.sign(p_E)*np.sqrt(abs(p_E))
 def u2p_n(u_n, E, l, particle_pair:Particle_Pair):
-    P = FofE_recursive(E, particle_pair.ac, particle_pair.M, particle_pair.m, l)
-    return 2000*P*u_n*abs(u_n)
+    _, P, _, _ = FofE_recursive(E, particle_pair.ac, particle_pair.M, particle_pair.m, l)
+    return 2000*P[-1,:]*u_n*abs(u_n)
 def p2u_n(p_n, E, l, particle_pair:Particle_Pair):
-    P = FofE_recursive(E, particle_pair.ac, particle_pair.M, particle_pair.m, l)
-    return np.sign(p_n)*np.sqrt(abs(p_n)/(2000 * P))
+    _, P, _, _ = FofE_recursive(E, particle_pair.ac, particle_pair.M, particle_pair.m, l)
+    return np.sign(p_n)*np.sqrt(abs(p_n)/(2000 * P[-1,:]))
 def u2p_g(u_g):
     return 2000*u_g*abs(u_g)
 def p2u_g(p_g):
@@ -27,14 +27,21 @@ def p2u_g(p_g):
 def du_dp_E(p_E):
     return 0.5 / np.sqrt(p_E)
 def du_dp_n(p_n, E, l, particle_pair:Particle_Pair):
-    P = FofE_recursive(E, particle_pair.ac, particle_pair.M, particle_pair.m, l)
-    return 0.5 / np.sqrt(2000 * P * p_n)
+    _, P, _, _ = FofE_recursive(E, particle_pair.ac, particle_pair.M, particle_pair.m, l)
+    return np.sign(p_n) * 0.5 / np.sqrt(2000 * P[-1,:] * abs(p_n))
 def du_dp_g(p_g):
-    return 0.5 / np.sqrt(2000 * p_g)
+    return np.sign(p_g) * 0.5 / np.sqrt(2000 * abs(p_g))
 
-def convert_deriv_du2dp(df_du: np.ndarray, 
-                        ladder_df: pd.DataFrame,
-                        Ta_pair: Particle_Pair):
+# Conversions with respect to gn2 and gg2:
+def u2g2(u):
+    return 1000*u*abs(u)
+def g22u(g2):
+    return np.sign(g2) * np.sqrt(1e-3 * abs(g2))
+def du_dg2(g2):
+    return np.sign(g2) * 0.5/np.sqrt(1000 * abs(g2))
+
+def convert_deriv_du2dp(df_du: np.array, 
+                        ladder_df: pd.DataFrame):
     """
     Converts derivatives df/du -> df/dp,
     
@@ -45,4 +52,40 @@ def convert_deriv_du2dp(df_du: np.ndarray,
 
     """
 
-    raise NotImplementedError('...')
+    # print('Shape of input derivatives df/du: ', df_du.shape)
+
+    N_par = df_du.shape[1]
+    N_res = N_par // 3
+    # print('Npar = ', N_par)
+
+    # Initialize the du_dp matrix
+    du_dp = np.zeros((N_par,))
+
+    dp_names = []
+
+    # Iterate for every three parameters
+    for res_idx in range(N_res):
+        par_idx = 3*res_idx
+        # Extract the corresponding rows from ladder_df
+        row_num = res_idx
+
+        row = ladder_df.iloc[row_num]
+
+        # du_e/dE
+        du_dp[par_idx] = du_dp_E(row['E'])
+
+        dp_names.append(f'df/dE_{res_idx}')
+
+        # du_g/dGg
+        # du_dp[i+1, i+1] = 0.5 / np.sqrt(2 * row['Gg'] * 1e-3)
+        du_dp[par_idx+1] = du_dg2(row['gg2'])
+        dp_names.append(f'df/dGg_{res_idx}')
+
+        # du_n/dGnx
+        du_dp[par_idx+2] = du_dg2(row['gn2'])
+        dp_names.append(f'df/dGn_{res_idx}')
+
+    # Convert df/du to df/dp
+    df_dp = df_du * du_dp[np.newaxis, :]
+
+    return df_dp, du_dp, dp_names

ATARI/sammy_interface/sammy_functions.py
@@ -236,13 +236,13 @@ def readpds(pdsfilepath):
 
     # Return the final result
     res_dict = {
-        'NPAR': NPAR,
-        'U': U_values[:NPAR],
-        'DATA': data,
-        'DELD': deld,
-        'THEORY': theory,
+        'NPAR': NPAR, # number of parameters
+        'U': U_values[:NPAR], # u parameter values
+        'DATA': data, # experimental data values
+        'DELD': deld, # uncertainity on data
+        'THEORY': theory, # theoretical values
         'PARTIAL_DERIVATIVES': partial_derivatives, # G with respect to u parameters
-        'PAR_NAMES': par_names
+        'PAR_NAMES': par_names # the names of the u parameters
     }
     return res_dict
 
@@ -372,6 +372,7 @@ def write_sampar(df, pair, initial_parameter_uncertainty, filename, vary_parm=Fa
 # ################################################ ###############################################
 
 def write_estruct_file(Energies, filename):
+    'Generates a dummy data file for sammy'
     # print("WARNING: if 'twenty' is not specified in sammy.inp, the data file format will change.\nSee 'sammy_interface.write_estruct_file'")
     if np.all([isinstance(E,float) for E in Energies]):
         pass
@@ -517,7 +518,7 @@ def write_saminp(filepath,
     if rto.derivatives_only:
         alphanumeric.insert(2, "GENERATE PARTIAL DERivatives only")
     if use_IDC:
-            alphanumeric.append("USER-SUPPLIED IMPLICIT DATA COVARIANCE MATRIX")
+        alphanumeric.append("USER-SUPPLIED IMPLICIT DATA COVARIANCE MATRIX")
 
     with open(filepath,'r') as f:
         old_lines = f.readlines()
@@ -556,18 +557,14 @@ def write_saminp(filepath,
            
 
 def make_runDIR(sammy_runDIR):
-    if os.path.isdir(sammy_runDIR):
-        pass
-    else:
+    if not os.path.isdir(sammy_runDIR):
         os.mkdir(sammy_runDIR)
 
 def fill_runDIR_with_templates(input_template, input_name, sammy_runDIR):
-
     if os.path.basename(input_template) == input_template:
         template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sammy_templates', input_template)
     else:
         template_path = input_template
-
     shutil.copy(template_path, os.path.join(sammy_runDIR, input_name))
 
 
@@ -776,7 +773,7 @@ def check_inputs(sammyINP: SammyInputData, sammyRTO:SammyRunTimeOptions):
         if np.sum(sammyINP.resonance_ladder[["varyE", "varyGg", "varyGn1"]].values) == 0.0:
             raise ValueError("Bayes is set to True but no varied parameters.")
 
-def run_sammy(sammyINP: SammyInputData, sammyRTO:SammyRunTimeOptions):
+def run_sammy(sammyINP:SammyInputData, sammyRTO:SammyRunTimeOptions):
 
     sammyINP.resonance_ladder = copy(sammyINP.resonance_ladder)
     check_inputs(sammyINP, sammyRTO)
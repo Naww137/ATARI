Fixed cross-validation standard deviation and strength function calculation.

ATARI/AutoFit/cross_validation.py
@@ -34,9 +34,9 @@ def find_CV_scores(fold_results, use_MAD:bool=False):
             CV_train_score_std  = 1.4826*np.median(np.abs(objn_train - CV_train_score_mean), axis=0)
         else:
             CV_test_score_mean  = np.mean(objn_test)
-            CV_test_score_std   = np.std(objn_test) / np.sqrt(K_folds)
+            CV_test_score_std   = np.std(objn_test, ddof=1) / np.sqrt(K_folds)
             CV_train_score_mean = np.mean(objn_train)
-            CV_train_score_std  = np.std(objn_train) / np.sqrt(K_folds)
+            CV_train_score_std  = np.std(objn_train, ddof=1) / np.sqrt(K_folds)
         CV_test_scores[Nres]  = {'mean':CV_test_score_mean , 'std':CV_test_score_std }
         CV_train_scores[Nres] = {'mean':CV_train_score_mean, 'std':CV_train_score_std}
     return CV_test_scores, CV_train_scores

ATARI/PiTFAll/strength_function_comparison.py
@@ -19,7 +19,23 @@ def strength_function_estimation(ladder:pd.DataFrame, energy_range:tuple, partic
         gJ = gstat(abs(Jpi), particle_pair.I, particle_pair.i)
         ladder_sg = ladder_windowed[ladder_windowed['J_ID'] == J_ID]
         gn2 = particle_pair.Gn_to_gn2(ladder_sg['Gn1'], ladder_sg['E'], l)
-        Sn_Jpi = gJ/((2*l+1)*dE) * np.sum(gn2)
+        Sn_Jpi = gJ/((2*l+1)*dE) * np.sum(np.abs(gn2)*1e-3)
+        Sns[Jpi] = Sn_Jpi
+    return Sns
+
+def expected_strength(particle_pair:Particle_Pair):
+    """
+    ...
+    """
+
+    Sns = {}
+    for Jpi, spingroup in particle_pair.spin_groups.items():
+        J_ID = spingroup['J_ID']
+        l = spingroup['Ls'][0]
+        gJ = gstat(abs(Jpi), particle_pair.I, particle_pair.i)
+        gn2m = spingroup['<gn2>']
+        Dm = spingroup['<D>']
+        Sn_Jpi = gJ/(2*l+1) * (gn2m*1e-3/Dm)
         Sns[Jpi] = Sn_Jpi
     return Sns
 